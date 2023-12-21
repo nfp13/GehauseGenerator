2eb37a36a318c42d7c9859fd6660b8340f8142d3
MaximStz <maximstoetzer@web.de>
2023-12-21 14:54:27 +0100

    Design

--- GehäuseGenerator/Form1.cs
+++ GehäuseGenerator/Form1.cs
@@ -21,5 +21,15 @@ namespace GehäuseGenerator
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

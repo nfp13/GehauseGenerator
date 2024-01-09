60f3f4ad202b8d9f2edb253fe1dc1960f9524f00
MaximStz <maximstoetzer@web.de>
2024-01-09 15:16:28 +0100

    fff

--- GehäuseGenerator/Form1.Designer.cs
+++ GehäuseGenerator/Form1.Designer.cs
@@ -164,6 +164,10 @@
             // 
             this.comboBox1.BackColor = System.Drawing.SystemColors.Control;
             this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "FotoX",
+            "FotoY",
+            "FotoZ"});
             this.comboBox1.Location = new System.Drawing.Point(210, 102);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(121, 21);

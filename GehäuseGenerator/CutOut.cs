27583489f9efb5b86472bd4ae7817aa804a37c22
nfp13 <nikolas.plett@icloud.com>
2024-01-10 22:20:09 +0100

    Added CutOut Class

---
+++ GehäuseGenerator/CutOut.cs
@@ -0,0 +1,42 @@
+﻿namespace GehäuseGenerator
+{
+    public class CutOut
+    {
+        #region Properties
+        public bool Top { get; set; }       //true if top
+        public bool Connector { get; set; } //true if connector, false if LED or Display
+
+        public double XP { get; set; }      //Coordianates of centerpoint of cutout from center of the Board
+        public double YP { get; set; }
+        public double ZP { get; set; }
+
+
+        public double XS { get; set; }      //Size of the Cutout in X, Y and Z direction
+        public double YS { get; set; }
+        public double ZS { get; set; }
+
+        public string Name { get; set; }
+
+        public int Index { get; set; }
+        #endregion
+
+        #region Constructors
+        public CutOut(string name, int index)
+        {
+            Name = name;
+            Index = index;
+        }
+        #endregion
+
+        #region Overrides
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", Index, Name);
+        }
+        #endregion
+
+    }
+
+}
+    
+

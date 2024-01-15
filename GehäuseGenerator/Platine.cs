5a777330b794b219d1cee93aa638b306922d6ca0
nfp13 <nikolas.plett@icloud.com>
2024-01-15 20:45:04 +0100

    Added Comments

--- GehäuseGenerator/Platine.cs
+++ GehäuseGenerator/Platine.cs
@@ -10,15 +10,18 @@ namespace GehäuseGenerator
     {
         public Platine(Inventor.Application inventorApp, string filePath, Status status)
         {
+            //Setting private variables
+
             _status = status;
+            _filePath = filePath;
+            _inventorApp = inventorApp;
 
             _status.Name = "Opening Assembly";
             _status.OnProgess();
 
-            _filePath = filePath;
-            _inventorApp = inventorApp;
+            //Opening the PCB assembly Document
 
-            _assemblyDocument = _inventorApp.Documents.Open(_filePath, true) as AssemblyDocument;
+            _assemblyDocument = _inventorApp.Documents.Open(_filePath, true) as AssemblyDocument;       
             _assemblyComponentDefinition = _assemblyDocument.ComponentDefinition;
 
             _status.Name = "Done";
@@ -28,6 +31,8 @@ namespace GehäuseGenerator
 
         public void Analyze()
         {
+            //Creating list containing all component occurence names
+
             Parts.Clear();
             int NumberOfOccurrences = _assemblyComponentDefinition.Occurrences.Count;
             int CurrentOccurrence = 1;
@@ -54,9 +59,10 @@ namespace GehäuseGenerator
 
             foreach (ComponentOccurrence componentOccurrence in _assemblyComponentDefinition.Occurrences)
             {
+                //Finding Board Assembly and calculating size, coordinates of centerpoint, holediameter and radius of corner
+
                 if (componentOccurrence.Name == boardOccurrenceName)
                 {
-                    //MessageBox.Show("boardFound");
                     Box box = componentOccurrence.Definition.RangeBox;
 
                     _MinPointBoard = box.MinPoint;
@@ -90,31 +96,28 @@ namespace GehäuseGenerator
                             {
                                 HoleDia = holeFeature.HoleDiameter.Value;
 
-                                Inventor.Point point = holeFeature.RangeBox.MinPoint;
+                                Inventor.Point minPointHole = holeFeature.RangeBox.MinPoint;
 
-                                if (_MinPointBoard.VectorTo(point).X + HoleDia / 2 < CornerRadius || CornerRadius == 0)
+                                if (_MinPointBoard.VectorTo(minPointHole).X + HoleDia / 2 < CornerRadius || CornerRadius == 0)
                                 {
-                                    CornerRadius = _MinPointBoard.VectorTo(point).X + HoleDia / 2;
+                                    CornerRadius = _MinPointBoard.VectorTo(minPointHole).X + HoleDia / 2;
                                 }
-
                             }
-                            //MessageBox.Show(CornerRadius.ToString("0.00") + " ; " + HoleDia.ToString("0.00"));
                         }
                     }
-
                 }
                 _status.Progress = 100;
                 _status.Name = "Done";
                 _status.OnProgess();
             }
 
+            //Calculating component height for bottom and top of PCB
+
             _MaxPointGes = _assemblyComponentDefinition.RangeBox.MaxPoint;
             _MinPointGes = _assemblyComponentDefinition.RangeBox.MinPoint;
 
             CompHeightBottom = _MinPointGes.VectorTo(_MinPointBoard).Z;
             CompHeightTop = _MaxPointBoard.VectorTo(_MaxPointGes).Z;
-
-            //MessageBox.Show(CompHeightTop.ToString("0.00") + " ; " + CompHeightBottom.ToString());
 
             _status.Name = "Done";
             _status.OnProgess();
@@ -123,6 +126,8 @@ namespace GehäuseGenerator
 
         public Matrix GetTransformationMatrix()
         {
+            //Creating transformation Matrix for placing the PCB-centerpoint in the origin of an assembly document
+
             Vector MinMaxVector = _MinPointBoard.VectorTo(_MaxPointBoard);
             MinMaxVector.ScaleBy(0.5);
             Inventor.Point MiddlePointPlatine = _MinPointBoard;
@@ -137,8 +142,12 @@ namespace GehäuseGenerator
 
         public void AddConnectorToCutOuts(string CutOutOccurrenceName)
         {
+            //Adding Connector to CutOuts list and getting all nessesary variables
+
             foreach (ComponentOccurrence componentOccurrence in _assemblyComponentDefinition.Occurrences)
             {
+                //Finding specific occurence
+
                 if (componentOccurrence.Name == CutOutOccurrenceName)
                 {
                     Box box = componentOccurrence.Definition.RangeBox;
@@ -150,25 +159,27 @@ namespace GehäuseGenerator
 
                     _CutOut.Connector = true;
 
+                    //Calculating size of the connector in x, y and z direction
+
                     _CutOut.XS = box.MaxPoint.X - box.MinPoint.X;
                     _CutOut.YS = box.MaxPoint.Y - box.MinPoint.Y;
                     _CutOut.ZS = box.MaxPoint.Z - box.MinPoint.Z;
+
+                    //Calculating location of connector-centerpoint relative to board-centerpoint in x, y and z direction
 
                     _CutOut.XP = box.MinPoint.X + (_CutOut.XS / 2) - _XOM;
                     _CutOut.YP = box.MinPoint.Y + (_CutOut.YS / 2) - _YOM;
                     _CutOut.ZP = box.MinPoint.Z + (_CutOut.ZS / 2) - _ZOM;
 
-                    //MessageBox.Show(box.MinPoint.Z.ToString("0.00") + " " + _ZOM.ToString("0.00") + box.MaxPoint.Z.ToString("0.00") + " ");
+                    //Checking if connector is located on the top or the bottom of the PCB
 
                     if (Top.Z <= 0.0)
                     {
                         _CutOut.Top = true;
-                        //MessageBox.Show("Oben");
                     }
                     else if (Bottom.Z >= 0.0)
                     {
                         _CutOut.Top = false;
-                        //MessageBox.Show("Unten");
                     }
                     else
                     {
@@ -176,11 +187,6 @@ namespace GehäuseGenerator
                     }
 
                     _CutOut.Name = componentOccurrence.Name;
-
-                    //MessageBox.Show(_CutOut.XP.ToString("0.00") + " " + _CutOut.YP.ToString("0.00") + " " + _CutOut.ZP.ToString("0.00"));
-                    //MessageBox.Show(_CutOut.XS.ToString("0.00") + " " + _CutOut.YS.ToString("0.00") + " " + _CutOut.ZS.ToString("0.00"));
-
-
                     CutOuts.Add(_CutOut);
                 }
             }
@@ -188,8 +194,12 @@ namespace GehäuseGenerator
 
         public void AddLEDToCutOuts(string CutOutOccurrenceName)
         {
+            //Adding LED/Display to CutOuts list and getting all nessesary variables
+
             foreach (ComponentOccurrence componentOccurrence in _assemblyComponentDefinition.Occurrences)
             {
+                //Finding specific occurence
+
                 if (componentOccurrence.Name == CutOutOccurrenceName)
                 {
                     Box box = componentOccurrence.Definition.RangeBox;
@@ -201,24 +211,27 @@ namespace GehäuseGenerator
 
                     _CutOut.Connector = false;
 
+                    //Calculating size of the connector in x, y and z direction
+
                     _CutOut.XS = box.MaxPoint.X - box.MinPoint.X;
                     _CutOut.YS = box.MaxPoint.Y - box.MinPoint.Y;
                     _CutOut.ZS = box.MaxPoint.Z - box.MinPoint.Z;
+
+                    //Calculating location of connector-centerpoint relative to board-centerpoint in x, y and z direction
 
                     _CutOut.XP = box.MinPoint.X + (_CutOut.XS / 2) - _XOM;
                     _CutOut.YP = box.MinPoint.Y + (_CutOut.YS / 2) - _YOM;
                     _CutOut.ZP = box.MinPoint.Z + (_CutOut.ZS / 2) - _ZOM;
 
+                    //Checking if connector is located on the top or the bottom of the PCB
 
                     if (Top.Z <= 0.0)
                     {
                         _CutOut.Top = true;
-                        //MessageBox.Show("Oben");
                     }
                     else if (Bottom.Z >= 0.0)
                     {
                         _CutOut.Top = false;
-                        //MessageBox.Show("Unten");
                     }
                     else
                     {
@@ -226,7 +239,6 @@ namespace GehäuseGenerator
                     }
 
                     _CutOut.Name = componentOccurrence.Name;
-
                     CutOuts.Add(_CutOut);
                 }
             }
@@ -234,9 +246,13 @@ namespace GehäuseGenerator
 
         public void SavePictureAs(string Path)
         {
+            //Taking picture of PCB and saving in Path
+
             _status.Name = "Taking Screenshot";
             _status.Progress = 0;
             _status.OnProgess();
+
+            //Creating the camera
 
             Inventor.View view = _assemblyDocument.Views[1];
             Inventor.Camera camera = view.Camera;
@@ -244,6 +260,8 @@ namespace GehäuseGenerator
 
             _status.Progress = 75;
             _status.OnProgess();
+
+            //Setting camera perspective, fiting the camera to the PCB and exporting picture
 
             camera.ViewOrientationType = ViewOrientationTypeEnum.kIsoTopRightViewOrientation;
             camera.Fit();
@@ -264,11 +282,10 @@ namespace GehäuseGenerator
 
         public List<string> Parts = new List<string>();
 
-        public double BoardW, BoardL, BoardH;               //Grundmaße der Platine
-        public double CompHeightTop, CompHeightBottom;      //Höhe der Komponenten auf der Platine
-        public double HoleDia, CornerRadius = 0;                //Durchmesser der Bohrungslöcher und Rundungsradius der Platine
-
-        private double _XOM, _YOM, _ZOM;           //Coordinates of centerpoint of Board
+        public double BoardW, BoardL, BoardH;                       //Grundmaße der Platine
+        public double CompHeightTop, CompHeightBottom;              //Höhe der Komponenten auf der Platine
+        public double HoleDia, CornerRadius = 0;                    //Durchmesser der Bohrungslöcher und Rundungsradius der Platine
+        private double _XOM, _YOM, _ZOM;                            //Coordinates of centerpoint of Board
 
         private Inventor.Point _MinPointBoard;
         private Inventor.Point _MaxPointBoard;
@@ -278,9 +295,5 @@ namespace GehäuseGenerator
         public List<CutOut> CutOuts = new List<CutOut>();
 
         private Status _status;
-
     }
-
 }
-    
-
